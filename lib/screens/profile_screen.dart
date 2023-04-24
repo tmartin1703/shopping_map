7b21dc4833f2a8c3b28bcbc273850a5fa0865231
Tóth Martin <tmartin1703@gmail.com>
2023-04-24 20:20:58 +0200

    add GestureDetectors

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -26,49 +26,50 @@ class ProfileScreen extends StatelessWidget {
         const SizedBox(
           height: 32.0,
         ),
-        Row(
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(left: 24.0),
-              child: CircleAvatar(
-                backgroundColor: Colors.white,
-                backgroundImage: NetworkImage(
-                    "https://static-00.iconduck.com/assets.00/user-avatar-icon-512x512-vufpcmdn.png"),
+        GestureDetector(
+          onTap: () { /* TODO */},
+          child: Row(
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(left: 24.0),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(
+                      "https://static-00.iconduck.com/assets.00/user-avatar-icon-512x512-vufpcmdn.png"),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: SizedBox(
-                      child: Text("John Doe",
-                          style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF474D53))),
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: SizedBox(
+                        child: Text("John Doe",
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF474D53))),
+                      ),
                     ),
-                  ),
-                  Text("john.doe@gmail.com",
-                      style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0XFF89909E))),
-                ],
+                    Text("john.doe@gmail.com",
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0XFF89909E))),
+                  ],
+                ),
               ),
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(right: 24.0),
-              child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.arrow_forward_ios,
-                  )),
-            )
-          ],
+              const Spacer(),
+              const Padding(
+                padding: EdgeInsets.only(right: 24.0),
+                child: Icon(
+                  Icons.arrow_forward_ios,
+                ),
+              )
+            ],
+          ),
         ),
         const SizedBox(height: 32),
         const Align(
@@ -86,27 +87,33 @@ class ProfileScreen extends StatelessWidget {
           scrollDirection: Axis.vertical,
           shrinkWrap: true,
           padding: const EdgeInsets.all(8),
-          children: const <Widget>[
-            ListTile(
-                title: Text("Past shopping routes",
-                    style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF474D53))),
-                leading: Icon(
-                  Icons.shopping_cart,
-                  color: Color(0xFF474D53),
-                )),
-            ListTile(
-                title: Text("Grocery list archive",
-                    style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF474D53))),
-                leading: Icon(
-                  Icons.assessment,
-                  color: Color(0xFF474D53),
-                )),
+          children: <Widget>[
+            GestureDetector(
+              onTap: () { /* TODO */},
+              child: const ListTile(
+                  title: Text("Past shopping routes",
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF474D53))),
+                  leading: Icon(
+                    Icons.shopping_cart,
+                    color: Color(0xFF474D53),
+                  )),
+            ),
+            GestureDetector(
+              onTap: () { /* TODO */},
+              child: const ListTile(
+                  title: Text("Grocery list archive",
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF474D53))),
+                  leading: Icon(
+                    Icons.assessment,
+                    color: Color(0xFF474D53),
+                  )),
+            ),
           ],
         ),
         const Align(
@@ -124,17 +131,20 @@ class ProfileScreen extends StatelessWidget {
           scrollDirection: Axis.vertical,
           shrinkWrap: true,
           padding: const EdgeInsets.all(8),
-          children: const <Widget>[
-            ListTile(
-                title: Text("Notifications",
-                    style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF474D53))),
-                leading: Icon(
-                  Icons.notifications,
-                  color: Color(0xFF474D53),
-                )),
+          children: <Widget>[
+            GestureDetector(
+              onTap: () { /* TODO */},
+              child: const ListTile(
+                  title: Text("Notifications",
+                      style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF474D53))),
+                  leading: Icon(
+                    Icons.notifications,
+                    color: Color(0xFF474D53),
+                  )),
+            ),
           ],
         ),
       ],
